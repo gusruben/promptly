8c31453b55e501c16e170b6e6920f676648a26ec
Gus Ruben <gusruben@protonmail.com>
2024-08-25 02:59:42 -0400

    Continue theming work & format

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,11 +21,7 @@ export const flyAndScale = (
 	const style = getComputedStyle(node);
 	const transform = style.transform === "none" ? "" : style.transform;
 
-	const scaleConversion = (
-		valueA: number,
-		scaleA: [number, number],
-		scaleB: [number, number]
-	) => {
+	const scaleConversion = (valueA: number, scaleA: [number, number], scaleB: [number, number]) => {
 		const [minA, maxA] = scaleA;
 		const [minB, maxB] = scaleB;
 
@@ -35,9 +31,7 @@ export const flyAndScale = (
 		return valueB;
 	};
 
-	const styleToString = (
-		style: Record<string, number | string | undefined>
-	): string => {
+	const styleToString = (style: Record<string, number | string | undefined>): string => {
 		return Object.keys(style).reduce((str, key) => {
 			if (style[key] === undefined) return str;
 			return str + `${key}:${style[key]};`;
@@ -47,16 +41,16 @@ export const flyAndScale = (
 	return {
 		duration: params.duration ?? 200,
 		delay: 0,
-		css: (t) => {
+		css: t => {
 			const y = scaleConversion(t, [0, 1], [params.y ?? 5, 0]);
 			const x = scaleConversion(t, [0, 1], [params.x ?? 0, 0]);
 			const scale = scaleConversion(t, [0, 1], [params.start ?? 0.95, 1]);
 
 			return styleToString({
 				transform: `${transform} translate3d(${x}px, ${y}px, 0) scale(${scale})`,
-				opacity: t
+				opacity: t,
 			});
 		},
-		easing: cubicOut
+		easing: cubicOut,
 	};
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
-<div class="w-full h-full flex flex-row p-6">
-    <div class=""></div>
+<div class="flex h-full w-full flex-row p-6">
+	<div class="flex flex-col gap-6">
+		<div class="bg-dark-900 rounded-lg"></div>
+	</div>
 </div>
 
 <style lang="postcss">
-	:global(body), :global(html) {
-		@apply w-full h-full;
+	:global(body),
+	:global(html) {
+		@apply h-full w-full;
 	}
 </style>
