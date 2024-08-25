8f8d789b1a587bf63569a7e2479e261c1d843df4
Gus Ruben <gusruben@protonmail.com>
2024-08-25 03:32:42 -0400

    Start on tasklist

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
+<script>
+	import * as TaskList from "$lib/components/ui/tasklist";
+</script>
+
 <div class="flex h-full w-full flex-row items-stretch p-6">
 	<div class="flex h-full flex-grow flex-col gap-6">
-		<div class="bg-background-2 flex-grow rounded-lg">
-            <p class="p-5 font-bold text-lg">Tasks</p>
-            <div class="flex-col w-full border-t-2"></div>
-        </div>
+		<div class="flex-grow rounded-lg bg-background-2">
+			<p class="p-5 text-lg font-bold">Tasks</p>
+			<!-- <div class="flex-col w-full border-t-2"></div> -->
+			<TaskList.Root class="border-t-2">
+				<TaskList.Item value="task-1">
+					<TaskList.Trigger>
+                        Task 1
+                    </TaskList.Trigger>
+					<TaskList.Content>Task 1 content</TaskList.Content>
+				</TaskList.Item>
+			</TaskList.Root>
+		</div>
 	</div>
 </div>
 
